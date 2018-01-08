837e56b890ab3a5ff374fab2de2163a133238f05
unknown <gzhou2@stuy.edu>
2018-01-07 21:41:44 -0500

    created frame for Board

--- GameBoard.java
+++ GameBoard.java
@@ -5,8 +5,20 @@ import java.awt.KeyEvent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.Timer;
-public class GameBoard{
-
+public class GameBoardextends JFrame {
+    private int BOARD_WIDTH;
+    private int BOARD_LENGTH;
+    private Timer timer;
+    private boolean isFalling;
+    private Shapes current
     
+    public Board(){
+	current = new Shape();
+	timer = new Timer(400,this);
+
+    }
+
+    public void actionPerformed(ActionEvent e){
+    }
 
 }
