d2b459116009c35d43497a985f5f718ea41843e3
GarveyZhou <gzhou2@stuy.edu>
2018-01-09 15:27:04 -0500

    Added paint

--- GameBoard.java
+++ GameBoard.java
@@ -1,42 +1,40 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
-import java.awt.KeyEvent;
+import java.awt.event.KeyEvent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 import java.awt.*;
-public class GameBoard extends JFrame {
-    private int BOARD_WIDTH;
-    private int BOARD_LENGTH;
+public class GameBoard extends JFrame implements ActionListener{
+    private static final int BOARD_WIDTH = 10;
+    private static final int BOARD_LENGTH = 20;
     private Timer timer;
     private boolean isFalling;
     private Shapes current;
 
     
     public GameBoard(){
-	BOARD_WIDTH = 10;
-	BOARD_LENGTH = 20;
+	Timer timer;
 	this.setSize(600,400);
 	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-	current = new Shape();
-	timer = new Timer(400,this);
-	
+	current = new Shapes();
+	timer = new Timer(400, this);
+	timer.start();
     }
 
+    private void clearBoard(){
+    }
+    
     public void actionPerformed(ActionEvent e){
     }
 
     public void paint(Graphics g){
 	super.paint(g);
-
-	for (int i = 0; i < BOARD_WIDTH; i ++){
-	    for(int x = 0; x< BOARD_LENGTH; x++){
-		g.fillRect(5,5,5,5);
-	    }
+	g.fillRect(0,0,10,20);
+	    
 	}
-    }
-    
+
     public static void main(String[] args){
 	GameBoard a = new Gameboard();
 	a.setvisible(true);
