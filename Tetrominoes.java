fb9817f0884c2769242faa61f82afa8db33333e2
shuxinwu <swu10@stuy.edu>
2018-01-09 20:34:12 -0500

    shape names

--- Tetrominoes.java
+++ Tetrominoes.java
@@ -1,61 +1,53 @@
 import java.awt.Color;
 
-/*
-enum Tetrominoes{
-    ZShape(new int[][]{{0,-1},{0,0},{-1,0},{1,-1}}),
-    SShape(new int[][]{{0,-1},{0,0},{1,0},{-1,-1}}),
-    TShape(new int[][]{{0,-1},{0,0},{1,0},{0,1}}),
-    IShape(new int[][]{{0,-1},{0,0},{0,1},{0,2}}),
-    SquareShape(new int[][]{{0,0},{1,0},{0,1},{1,1}}),
-    Lshape(new int[][]{{1,-1},{0,-1},{0,0},{0,1}}),
-    MirrorLShape(new int[][]{{-1,-1},{0,-1},{0,0},{0,1}});
-    public int[][] coords;
-
-    private Tetrominoes(int[][] coords ){
-	this.coords = coords;
-    }
-*/
 public class Tetrominoes{
 
   // shapes in the order above
   // all four orientations, clockwise
   private Point[][][] Tetrominoes = {
+    // Z shape
     {
         { new Point(0, 0), new Point(1, 0), new Point(1, 1), new Point(2, 1) },
         { new Point(1, 0), new Point(0, 1), new Point(1, 1), new Point(0, 2) },
         { new Point(0, 0), new Point(1, 0), new Point(1, 1), new Point(2, 1) },
         { new Point(1, 0), new Point(0, 1), new Point(1, 1), new Point(0, 2) }
     },
+    // S shape
     {
        { new Point(1, 0), new Point(2, 0), new Point(0, 1), new Point(1, 1) },
         { new Point(0, 0), new Point(0, 1), new Point(1, 1), new Point(1, 2) },
         { new Point(1, 0), new Point(2, 0), new Point(0, 1), new Point(1, 1) },
         { new Point(0, 0), new Point(0, 1), new Point(1, 1), new Point(1, 2) }
     },
+    // T shape
     {
       { new Point(1, 0), new Point(0, 1), new Point(1, 1), new Point(2, 1) },
         { new Point(1, 0), new Point(0, 1), new Point(1, 1), new Point(1, 2) },
         { new Point(0, 1), new Point(1, 1), new Point(2, 1), new Point(1, 2) },
         { new Point(1, 0), new Point(1, 1), new Point(2, 1), new Point(1, 2) }
     },
+    // I shape
     {
       { new Point(0, 1), new Point(1, 1), new Point(2, 1), new Point(3, 1) },
         { new Point(1, 0), new Point(1, 1), new Point(1, 2), new Point(1, 3) },
         { new Point(0, 1), new Point(1, 1), new Point(2, 1), new Point(3, 1) },
         { new Point(1, 0), new Point(1, 1), new Point(1, 2), new Point(1, 3) }
     },
+    // square
     {
       { new Point(0, 0), new Point(0, 1), new Point(1, 0), new Point(1, 1) },
         { new Point(0, 0), new Point(0, 1), new Point(1, 0), new Point(1, 1) },
         { new Point(0, 0), new Point(0, 1), new Point(1, 0), new Point(1, 1) },
         { new Point(0, 0), new Point(0, 1), new Point(1, 0), new Point(1, 1) }
     },
+    // L shape
     {
       { new Point(0, 1), new Point(1, 1), new Point(2, 1), new Point(2, 2) },
         { new Point(1, 0), new Point(1, 1), new Point(1, 2), new Point(0, 2) },
         { new Point(0, 1), new Point(1, 1), new Point(2, 1), new Point(0, 0) },
         { new Point(1, 0), new Point(1, 1), new Point(1, 2), new Point(2, 0) }
     },
+    // J shape
     {
       { new Point(0, 1), new Point(1, 1), new Point(2, 1), new Point(2, 0) },
         { new Point(1, 0), new Point(1, 1), new Point(1, 2), new Point(2, 2) },
@@ -65,13 +57,15 @@ public class Tetrominoes{
 
   }
 
-    private Color[] tetroColors = { Color.red, Color.green, Color.pink, Color.cyan, Color.yellow, Color.orange, Color.blue };
+  private Color[] tetroColors = { Color.red, Color.green, Color.pink, Color.cyan, Color.yellow, Color.orange, Color.blue };
 
-    // +1 clockwise -1 counterclockwise
+  // +1 clockwise -1 counterclockwise
+  public void rotate(int a){
+    if (a < 0){
 
-    public void rotate(int a){
-      if (a < 0){
-
-      }
     }
+  }
+
+  // rand piece rand orientation
+
 }
