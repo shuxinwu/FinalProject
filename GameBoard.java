05691bf6514ec888ea767caa6f25d941a13ab990
unknown <gzhou2@stuy.edu>
2018-01-09 21:45:59 -0500

    Created GUI

--- GameBoard.java
+++ GameBoard.java
@@ -1,43 +1,38 @@
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.Timer;
+import java.awt.event.*;
+import javax.swing.*;
 import java.awt.*;
+
 public class GameBoard extends JFrame implements ActionListener{
     private static final int BOARD_WIDTH = 10;
     private static final int BOARD_LENGTH = 20;
-    private Timer timer;
-    private boolean isFalling;
-    private Shapes current;
+    private DrawCanvas canvas;
 
     
     public GameBoard(){
-	Timer timer;
-	this.setSize(600,400);
-	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-	current = new Shapes();
-	timer = new Timer(400, this);
-	timer.start();
+	canvas = new DrawCanvas();
+	canvas.setPreferredSize(new Dimension(200,400));
+	Container c = getContentPane();
+	this.setContentPane(canvas);
+	setDefaultCloseOperation(EXIT_ON_CLOSE);
+	setSize(300,600);
+	setTitle("Tetris");
+	setVisible(true);
     }
 
-    private void clearBoard(){
-    }
     
     public void actionPerformed(ActionEvent e){
     }
-
-    public void paint(Graphics g){
-	super.paint(g);
-	g.fillRect(0,0,10,20);
-	    
+    private class DrawCanvas extends JPanel{
+	public void paintComponent(Graphics g){
+	    super.paintComponent(g);
+	    g.setColor(Color.BLACK);
+	    g.fillRect(0,0,200,400);
 	}
+    }
+					     
 
     public static void main(String[] args){
-	GameBoard a = new Gameboard();
-	a.setvisible(true);
+	new GameBoard();
 	
     }
 }
