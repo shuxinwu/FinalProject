74ffc71596e1a49644dce8852d884108c8cf3c5b
unknown <gzhou2@stuy.edu>
2018-01-11 22:13:42 -0500

    moving shape

--- GameBoard.java
+++ GameBoard.java
@@ -7,17 +7,18 @@ public class GameBoard extends JFrame implements ActionListener{
     private static final int BOARD_WIDTH = 10;
     private static final int BOARD_LENGTH = 20;
     private DrawCanvas canvas;
+    private  Point  [][] Tetrominoes = {
+	{ new Point(0, 0), new Point(1, 0), new Point(1, 1), new Point(2, 1) },
+	{ new Point(1, 0), new Point(0, 1), new Point(1, 1), new Point(0, 2) },
+	{ new Point(0, 0), new Point(1, 0), new Point(1, 1), new Point(2, 1) },
+	{ new Point(1, 0), new Point(0, 1), new Point(1, 1), new Point(0, 2) }
+    };
+    private Timer timer;
     //  private Tetrominoes shape;
     
     public GameBoard(){
 	setFocusable(true);
 	//	shape = new Tetrominoes();
-	Point  [][] Tetrominoes = {
-      { new Point(0, 0), new Point(1, 0), new Point(1, 1), new Point(2, 1) },
-      { new Point(1, 0), new Point(0, 1), new Point(1, 1), new Point(0, 2) },
-      { new Point(0, 0), new Point(1, 0), new Point(1, 1), new Point(2, 1) },
-      { new Point(1, 0), new Point(0, 1), new Point(1, 1), new Point(0, 2) }
-	};
 	canvas = new DrawCanvas();
 	canvas.setPreferredSize(new Dimension(200,400));
 	Container c = getContentPane();
@@ -27,29 +28,41 @@ public class GameBoard extends JFrame implements ActionListener{
 	setTitle("Tetris");
 	setVisible(true);
 	//addKeyListener(new KeyAdapter());
-	}
-    public void actionPerformed(ActionEvent e){
+	ActionListener a = new ActionListener() {
+		public void actionPerformed(ActionEvent evt) {
+		    update();   
+		    repaint();  
+		}
+	    };
+	timer = new Timer(400,a);
+	timer.start();
+
+	    
     }
-    
+    public void actionPerformed(ActionEvent e){
+	//update();
+	//	repaint();	    
+    }
+    public void update(){
+	for (int x = 0 ; x < Tetrominoes[0].length;x++){
+	    Tetrominoes[0][x].translate(0,1);
+	}
+    }
+
     private class DrawCanvas extends JPanel{
 	public void paintComponent(Graphics g){
-	    Point  [][] Tetrominoes = {
-		{ new Point(0, 0), new Point(1, 0), new Point(1, 1), new Point(2, 1) },
-		{ new Point(1, 0), new Point(0, 1), new Point(1, 1), new Point(0, 2) },
-		{ new Point(0, 0), new Point(1, 0), new Point(1, 1), new Point(2, 1) },
-		{ new Point(1, 0), new Point(0, 1), new Point(1, 1), new Point(0, 2) }
-	    };
 	    super.paintComponent(g);
 	    g.setColor(Color.BLACK);
 	    g.fillRect(0,0,200,400);
 	    g.setColor(Color.RED);
-	    /* for(int x = 0; x < Tetrominoes[0].length;x++){
+	    for(int x = 0; x < Tetrominoes[0].length;x++){
 		g.fillRect(100-(int)Tetrominoes[0][x].getX() *20,(int)Tetrominoes[0][x].getY()*20, 20, 20);
-		}*/
+	    }
 	    
 	}
     }
     
+	
     /*
     public void keyPressed(KeyEvent e){
 	switch(evt.getKeyCode()){
@@ -59,11 +72,8 @@ public class GameBoard extends JFrame implements ActionListener{
     }
     */
 
-    private void moveDown(){
-    }
     public static void main(String[] args){
 	new GameBoard();
-	
     }
 }
  
