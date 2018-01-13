29317afcf9808d32eb93e56f9bed4dd491d44dc1
shuxinwu <swu10@stuy.edu>
2018-01-12 21:09:28 -0500

    keyListner testing

--- GameBoard.java
+++ GameBoard.java
@@ -2,37 +2,69 @@ import java.awt.event.*;
 import javax.swing.*;
 import java.awt.*;
 
-public class GameBoard extends JFrame implements ActionListener{
-    private static final int BOARD_WIDTH = 10;
-    private static final int BOARD_LENGTH = 20;
-    private DrawCanvas canvas;
-
+public class GameBoard extends JFrame implements KeyListener{
+  private static final int BOARD_WIDTH = 10;
+  private static final int BOARD_LENGTH = 20;
+  private DrawCanvas canvas;
     
-    public GameBoard(){
-	canvas = new DrawCanvas();
-	canvas.setPreferredSize(new Dimension(200,400));
-	Container c = getContentPane();
-	this.setContentPane(canvas);
-	setDefaultCloseOperation(EXIT_ON_CLOSE);
-	setSize(300,600);
-	setTitle("Tetris");
-	setVisible(true);
-    }
+  public GameBoard(){
+    canvas = new DrawCanvas();
+    canvas.setPreferredSize(new Dimension(200,400));
+    Container c = getContentPane();
+    this.setContentPane(canvas);
+    setDefaultCloseOperation(EXIT_ON_CLOSE);
+    setSize(300,600);
+    setTitle("Tetris");
+    setVisible(true);
 
-    
-    public void actionPerformed(ActionEvent e){
+    this.addKeyListener(this);
+  }
+
+  
+  public void keyTyped(KeyEvent e){}
+  public void keyReleased(KeyEvent e){}
+  public void keyPressed(KeyEvent e){
+    int a = e.getKeyCode();
+    if(a == KeyEvent.VK_UP){
+      //   rotate(1);
+      System.out.println("up");
     }
-    private class DrawCanvas extends JPanel{
-	public void paintComponent(Graphics g){
+    if(a == KeyEvent.VK_RIGHT){
+      //   move(1);
+      System.out.println("right");
+    }
+    if(a == KeyEvent.VK_LEFT){
+      //     move(-1);
+      System.out.println("left");
+    }
+    if(a == KeyEvent.VK_DOWN){
+      // faster move down
+      System.out.println("down");
+    }
+    if(a == KeyEvent.VK_SPACE){
+      // instant drop
+      System.out.println("space");
+    }
+    if(a == KeyEvent.VK_Z){
+      // rotate(-1);
+      System.out.println("z");
+    }
+  }
+
+  private class DrawCanvas extends JPanel{
+    public void paintComponent(Graphics g){
 	    super.paintComponent(g);
 	    g.setColor(Color.BLACK);
 	    g.fillRect(0,0,200,400);
-	}
     }
-					     
+  }
+  
+  
+  public static void main(String[] args){
+    new GameBoard();
+ 
+  }
 
-    public static void main(String[] args){
-	new GameBoard();
-	
-    }
+
+  
 }
